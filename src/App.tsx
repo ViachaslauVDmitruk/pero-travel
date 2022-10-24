d5b13ef9d52788786714bd949cde4fb94d4970e5
ViachaslauVDmitruk <staryxaizergil@rambler.ru>
2022-10-24 22:30:44 +0300

    one more time to fix error deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from "react";
 import "./App.css";
 import { Header } from "./components/header";
 
 function App() {
-  console.log("run");
   return (
     <div className="App">
       <Header />
