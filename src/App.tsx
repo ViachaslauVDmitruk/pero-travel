f732af7c2f3a13bdc64624b83ac9de73893d3d68
ViachaslauVDmitruk <staryxaizergil@rambler.ru>
2022-11-06 18:40:17 +0300

    added components to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.scss";
+
+import { AboutUs } from "./components/about-us";
+import { Excursions } from "./components/excursions";
 import { Header } from "./components/header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutUs />
+      <Excursions />
     </div>
   );
 }
