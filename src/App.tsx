44121b52757fc8109169f34ccbfba7c3e894fd49
ViachaslauVDmitruk <staryxaizergil@rambler.ru>
2022-11-07 18:46:36 +0300

    Created footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import "./App.scss";
 
 import { AboutUs } from "./components/about-us";
 import { Excursions } from "./components/excursions";
+import { Footer } from "./components/fotter";
 import { Header } from "./components/header";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <AboutUs />
+      {/* <AboutUs /> */}
       <Excursions />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/fotter/footer.tsx
@@ -0,0 +1,68 @@
+import logo from "./assets/logo-color.png";
+import fb from "./assets/fb.png";
+import insta from "./assets/insta.png";
+import vk from "./assets/vk.png";
+import mail from "./assets/mail.svg";
+import tg from "./assets/tg.svg";
+import wapp from "./assets/wapp.svg";
+
+import styles from "./footer.module.scss";
+import { NAV_LINKS } from "../../constans/header-nav-links";
+
+export const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.wrapper}>
+        <div className={styles.logo}>
+          <img src={logo} alt="img" />
+        </div>
+        <div className={styles.navLinks}>
+          <ul>
+            {NAV_LINKS.map(({ link, id }) => (
+              <li key={id}>{link}</li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.messangersItems}>
+          <div className={styles.messangersItem}>
+            <div className={styles.itemImage}>
+              <img src={wapp} alt="img" />
+            </div>
+            <div className={styles.itemNumber}>+7 964 944 18 74</div>
+          </div>
+          <div className={styles.messangersItem}>
+            <div className={styles.itemImage}>
+              <img src={tg} alt="img" />
+            </div>
+            <div className={styles.itemNumber}>+7 918 919 98 28</div>
+          </div>
+          <div className={styles.messangersItem}>
+            <div className={styles.itemImage}>
+              <img src={tg} alt="img" />
+            </div>
+            <div className={styles.itemNumber}>Телеграм-бот PeroTravel</div>
+          </div>
+        </div>
+        <div className={styles.social}>
+          <div className={styles.mail}>
+            <div className={styles.mailImage}>
+              <img src={mail} alt="img" />
+            </div>
+            <a href="mailto:office@perotravel.ru">office@perotravel.ru</a>
+          </div>
+          <div className={styles.socialIcons}>
+            <div className={styles.icon}>
+              <img src={vk} alt="img" />
+            </div>
+            <div className={styles.icon}>
+              <img src={insta} alt="img" />
+            </div>
+            <div className={styles.icon}>
+              <img src={fb} alt="img" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
