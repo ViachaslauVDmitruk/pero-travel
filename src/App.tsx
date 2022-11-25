4c2817b0af6b2b2a94689ede3b5d68514dc8784f
ViachaslauVDmitruk <staryxaizergil@rambler.ru>
2022-11-25 21:30:09 +0300

    Created a page "main" component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import "./App.scss";
+import { Main } from "./pages/main";
 
-import { AboutUs } from "./components/about-us";
-import { BlockRequest } from "./components/block-request";
-import { Excursions } from "./components/excursions";
-import { Footer } from "./components/fotter";
-import { Gallery } from "./components/gallery";
-import { Header } from "./components/header";
-import { SliderExcursions } from "./components/slider-excursions";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <SliderExcursions />
-      <AboutUs />
-      <Excursions />
-      <Gallery />
-      <BlockRequest />
-      <Footer />
+      <Main />
     </div>
   );
 }
---
+++ src/pages/main/main.tsx
@@ -0,0 +1,23 @@
+import { AboutUs } from "../../components/about-us";
+import { Excursions } from "../../components/excursions";
+import { Footer } from "../../components/fotter";
+import { Gallery } from "../../components/gallery";
+import { Header } from "../../components/header";
+import { SliderExcursions } from "../../components/slider-excursions";
+
+import styles from "./main.module.scss";
+
+export const Main = () => {
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <main className={styles.main}>
+        <SliderExcursions />
+        <AboutUs />
+        <Excursions />
+        <Gallery />
+      </main>
+      <Footer />
+    </div>
+  );
+};
