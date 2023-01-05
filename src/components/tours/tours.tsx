8eb64781d27c37340c0fbb0a4faddece75f4f433
ViachaslauVDmitruk <staryxaizergil@rambler.ru>
2023-01-06 00:08:25 +0300

    Deleted type props from tours component

--- src/components/tours/tours.tsx
+++ src/components/tours/tours.tsx
@@ -1,33 +1,18 @@
-import { TOURS } from "../../constans/tours";
-import { ToursProps } from "../../types/tours";
+import { useState } from "react";
 import { CardDescription } from "../card-description";
 import styles from "./tours.module.scss";
 
-export const Tours = ({
-  text,
-  title,
-  id,
-  image,
-  costAdult,
-  costChild,
-  duration,
-}: ToursProps) => {
+export const Tours = () => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [toursPerPage] = useState<number>(4);
+
+  const lastTourIndex = currentPage * toursPerPage;
+  const firstToutIndex = lastTourIndex - toursPerPage;
+
   return (
     <div className={styles.tours}>
       <div className={styles.title}>Наши туры</div>
-      {TOURS.map(
-        ({ text, title, id, image, costAdult, costChild, duration }) => (
-          <CardDescription
-            text={text}
-            title={title}
-            id={id}
-            image={image}
-            costAdult={costAdult}
-            costChild={costChild}
-            duration={duration}
-          />
-        )
-      )}
+      <CardDescription />
     </div>
   );
 };
